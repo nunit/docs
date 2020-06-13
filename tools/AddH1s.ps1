00ec24d52339425ac6b44b6a37e99c7aca7efa3b
Sean Killeen <SeanKilleen@gmail.com>
2020-06-13 08:22:11 -0400

    Beginning of H1 script

---
+++ tools/AddH1s.ps1
@@ -0,0 +1,27 @@
+# Prerequisite: Install-Module -Name PowerShellHumanizer
+# Run this script from within its directory; the paths are relative
+
+Import-Module PowerShellHumanizer
+
+$LINE_BREAK = "`r`n"
+
+function AddHeaderToSourceFile($fullPath) {
+    $fullPath
+
+    $fileName = [io.path]::GetFileNameWithoutExtension($fullPath)
+    $firstTenLines = Get-Content -Path $fullPath -First 10
+    
+    $linesStartingWithH1 = $firstTenLines | where { $_.ToString().StartsWith("# ")}
+    $linesStartingWithFrontmatter = $firstTenLines | where { $_ -ne $null -and $_.StartsWith("---")}
+    
+    $hasH1 = $linesStartingWithH1.Length -gt 0
+    $hasFrontmatter = $linesStartingWithFrontmatter.Length -gt 0
+    
+    if (!$hasH1){
+        $newH1 = $fileName.Humanize()
+        "New H1 would be $newH1"
+    }
+    
+}
+
+Get-ChildItem  ..\docs**\*.md -Recurse | Select FullName | ForEach-Object { AddHeaderToSourceFile($_.FullName)} 
