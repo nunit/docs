6f73e2b2891f2c160fe8da957e04f6447cd5364d
Sean Killeen <SeanKilleen@gmail.com>
2020-06-13 08:44:16 -0400

    Draft script

--- tools/AddH1s.ps1
+++ tools/AddH1s.ps1
@@ -21,6 +21,37 @@ function AddHeaderToSourceFile($fullPath) {
         $newH1 = $fileName.Humanize()
         "New H1 will be $newH1"
         
+        if(!$hasFrontmatter){
+            # Insert line first
+            $alreadyInserted = $false
+            (Get-Content $fullPath) |
+                    Foreach-Object {
+                        if(!$alreadyInserted){
+                            "# $newH1"
+                            $alreadyInserted = $true
+                        }
+                        $_ # send the current line to output
+                    } | Set-Content $fullPath
+        }else{
+            # Insert line after frontmatter
+            $count = 0
+            $alreadyInserted = $false
+            (Get-Content $fullPath) |
+                    Foreach-Object {
+                        $_ # send the current line to output
+                        if ($_ -match "---")
+                        {
+                            $count++
+                            if($count -ge 2 -and !$alreadyInserted){
+                                "# $newH1"
+                                $alreadyInserted = $true
+                            }
+                        }
+                    } | Set-Content $fullPath
+        }
+    }
+    else{
+        "$fullPath already has H1; skipping"
     }
     
 }
