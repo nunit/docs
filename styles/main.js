f19fac716f29fa9ff94c90540fefcad4f97f1f37
SeanKilleen <SeanKilleen@users.noreply.github.com>
2021-12-23 01:32:23 +0000

    update current year(copyright) (#621) b8e9b451793604c43072f072bc3115aeedfc2c0f

--- styles/main.js
+++ styles/main.js
@@ -1,1 +1,2 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+var currentYear= new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
