38c1003506858aad2c0341e357dfe47ff540d2da
Sean Killeen <SeanKilleen@gmail.com>
2023-01-06 13:15:18 -0500

    Make snippet clearer

--- docs/snippets/Snippets.NUnit/Assumptions.cs
+++ docs/snippets/Snippets.NUnit/Assumptions.cs
@@ -10,12 +10,12 @@ namespace Snippets.NUnit
     {
         #region TestThatUsesAssume
         [Test]
-        public void Number_Divided_By_ItselfIs_1()
+        public void Number_Divided_By_Itself_Is1()
         {
             var numberToCheck = 5;
             var divisor = GetMatchingDivisor(numberToCheck);
 
-            Assume.That(divisor, Is.Not.EqualTo(0), () => "divisor must not zero in order for this test to be correct");
+            Assume.That(divisor, Is.Not.EqualTo(0), () => "divisor must not zero in order for this test to be valid");
 
             var result = numberToCheck / divisor;
             Assert.That(result, Is.EqualTo(1));
