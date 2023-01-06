07ce3961dd01d506522b31f977a4d6c6e726a697
Sean Killeen <SeanKilleen@gmail.com>
2023-01-06 11:34:23 -0500

    Create basic sln + snippet project

---
+++ snippets/Snippets.NUnit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Snippets.NUnit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
