9e80f28cf574e972a66b6fe0e81b86b4c8b0cbdb
Sean Killeen <SeanKilleen@gmail.com>
2023-01-06 14:31:17 -0500

    AuthorAttribute

---
+++ docs/snippets/Snippets.NUnit/Attributes/AuthorAttribute.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Snippets.NUnit.Attributes
+{
+
+    public class AuthorAttributeExample
+    {
+        #region AuthorAttributeExample
+        [TestFixture]
+        [Author("Jane Doe", "jane.doe@example.com")]
+        [Author("Another Developer", "email@example.com")]
+        public class MyTests
+        {
+            [Test]
+            public void Test1() { /* ... */ }
+
+            [Test]
+            [Author("Joe Developer")]
+            [Author("Yet Another Developer", "not.my.email@example.com")]
+            public void Test2() { /* ... */ }
+        }
+
+        [TestFixture(Author = "Jane Doe")]
+        public class MyOtherTests
+        {
+            [Test]
+            public void Test1() { /* ... */ }
+
+            [Test(Author = "Joe Developer")]
+            public void Test2() { /* ... */ }
+        }
+        #endregion
+    }
+}
