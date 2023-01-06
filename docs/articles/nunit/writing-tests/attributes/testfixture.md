9a153312e525d6bf6b2ecf6e4400027f44df8a87
Sean Killeen <SeanKilleen@gmail.com>
2023-01-06 16:06:15 -0500

    more samples and cleanup

--- docs/articles/nunit/writing-tests/attributes/testfixture.md
+++ docs/articles/nunit/writing-tests/attributes/testfixture.md
@@ -136,53 +136,11 @@ and which are normal constructor parameters.
    arguments. Again, for this example, the type info is duplicated, but
    it is at least more cleanly separated from the normal arguments...
 
-```csharp
-[TestFixture(100.0, 42, TypeArgs=new Type[] { typeof(double), typeof(int) })]
-[TestFixture(42, 100.0, TypeArgs=new Type[] { typeof(int), typeof(double) })]
-public class SpecifyTypeArgsSeparately<T1, T2>
-{
-    T1 t1;
-    T2 t2;
-
-    public SpecifyTypeArgsSeparately(T1 t1, T2 t2)
-    {
-        this.t1 = t1;
-        this.t2 = t2;
-    }
-
-    [TestCase(5, 7)]
-    public void TestMyArgTypes(T1 t1, T2 t2)
-    {
-        Assert.That(t1, Is.TypeOf<T1>());
-        Assert.That(t2, Is.TypeOf<T2>());
-    }
-}
-```
+[!code-csharp[SpecifyTypeArgsSeparately](~/snippets/Snippets.NUnit/Attributes/TestFixtureAttributeExamples.cs#SpecifyTypeArgsSeparately)]
 
 * In some cases, when the constructor makes use of all the type parameters
    NUnit may simply be able to deduce them from the arguments provided.
    That's the case here and the following is the preferred way to
    write this example...
 
-```csharp
-[TestFixture(100.0, 42)]
-[TestFixture(42, 100.0)]
-public class DeduceTypeArgsFromArgs<T1, T2>
-{
-    T1 t1;
-    T2 t2;
-
-    public DeduceTypeArgsFromArgs(T1 t1, T2 t2)
-    {
-        this.t1 = t1;
-        this.t2 = t2;
-    }
-
-    [TestCase(5, 7)]
-    public void TestMyArgTypes(T1 t1, T2 t2)
-    {
-        Assert.That(t1, Is.TypeOf<T1>());
-        Assert.That(t2, Is.TypeOf<T2>());
-    }
-}
-```
+[!code-csharp[DeducedTypesFromArguments](~/snippets/Snippets.NUnit/Attributes/TestFixtureAttributeExamples.cs#DeducedTypesFromArguments)]
--- docs/snippets/Snippets.NUnit/Attributes/AuthorAttributeExamples.cs
+++ docs/snippets/Snippets.NUnit/Attributes/AuthorAttributeExamples.cs
@@ -103,6 +103,58 @@ namespace Snippets.NUnit.Attributes
             }
         }
         #endregion
+
+        #region SpecifyTypeArgsSeparately
+        [TestFixture(100.0, 42, TypeArgs = new[] { typeof(double), typeof(int) })]
+        [TestFixture(42, 100.0, TypeArgs = new[] { typeof(int), typeof(double) })]
+        public class SpecifyTypeArgsSeparately<T1, T2>
+        {
+            private readonly T1 _t1;
+            private readonly T2 _t2;
+
+            public SpecifyTypeArgsSeparately(T1 t1, T2 t2)
+            {
+                _t1 = t1;
+                _t2 = t2;
+            }
+
+            [TestCase(5, 7)]
+            public void TestMyArgTypes(T1 t1, T2 t2)
+            {
+                Assert.That(t1, Is.TypeOf<T1>());
+                Assert.That(t1, Is.LessThan(_t1));
+
+                Assert.That(t2, Is.TypeOf<T2>());
+                Assert.That(t2, Is.LessThan(_t2));
+            }
+        }
+        #endregion
+
+        #region DeducedTypesFromArguments
+        [TestFixture(100.0, 42)]
+        [TestFixture(42, 100.0)]
+        public class DeduceTypeArgsFromArgs<T1, T2>
+        {
+            private readonly T1 _t1;
+            private readonly T2 _t2;
+
+            public DeduceTypeArgsFromArgs(T1 t1, T2 t2)
+            {
+                _t1 = t1;
+                _t2 = t2;
+            }
+
+            [TestCase(5, 7)]
+            public void TestMyArgTypes(T1 t1, T2 t2)
+            {
+                Assert.That(t1, Is.TypeOf<T1>());
+                Assert.That(t1, Is.LessThan(_t1));
+
+                Assert.That(t2, Is.TypeOf<T2>());
+                Assert.That(t2, Is.LessThan(_t2));
+            }
+        }
+        #endregion
     }
     public class AuthorAttributeExamples
     {
