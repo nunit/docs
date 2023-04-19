a85a2728760bcbe6cfc757be86ceb034a6971e17
SeanKilleen <SeanKilleen@users.noreply.github.com>
2023-04-19 00:58:38 +0000

    Update docfx action to 2.4.0 (#751)
    * Update docfx action to 2.4.0
    
    * implement updated header template 33b944679cf53b27ef0fb650157dbb115bbfaa82

--- styles/main.js
+++ styles/main.js
@@ -1,1 +1,6 @@
-// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+var currentYear= new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
+
+// NUnit docs adds the lang attribute ourselves for accessibility reasons, as docfx does not do this out of the box.
+var htmlElement = document.querySelector("html");
+htmlElement.setAttribute("lang", "en");
