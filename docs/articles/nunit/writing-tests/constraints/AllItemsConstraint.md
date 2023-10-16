d5b9f0c96562fa63fcace77efdc0bcdb4e9375a1
Sean Killeen <SeanKilleen@gmail.com>
2023-10-16 01:51:23 +0000

    Add and use programmatic example

--- docs/articles/nunit/writing-tests/constraints/AllItemsConstraint.md
+++ docs/articles/nunit/writing-tests/constraints/AllItemsConstraint.md
@@ -10,18 +10,10 @@ AllItemsConstraint(Constraint itemConstraint)
 
 ## Syntax
 
-```csharp
-Is.All...
-Has.All...
-```
+You can use `Is.All` or `Has.All` to refer to all items in an `IEnumerable`.
 
 ## Examples of Use
 
-```csharp
-int[] iarray = new int[] { 1, 2, 3 };
-string[] sarray = new string[] { "a", "b", "c" };
-Assert.That(iarray, Is.All.Not.Null);
-Assert.That(sarray, Is.All.InstanceOf<string>());
-Assert.That(iarray, Is.All.GreaterThan(0));
-Assert.That(iarray, Has.All.GreaterThan(0));
-```
+[!code-csharp[IsBasedFormat](~/snippets/Snippets.NUnit/Constraints/AllItemsConstraintExamples.cs#AllItemsIsExample)]
+
+[!code-csharp[HasBasedFormat](~/snippets/Snippets.NUnit/Constraints/AllItemsConstraintExamples.cs#AllItemsHasExample)]
---
+++ docs/snippets/Snippets.NUnit/Constraints/AllItemsConstraintExamples.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+
+namespace Snippets.NUnit.Attributes
+{
+    public class AllItemsConstraintExamples
+    {
+        #region AllItemsIsExample
+        [Test]
+        public void CanUseIsToTalkAboutAllItems()
+        {
+            int[] arrayOfIntegers = new int[] { 1, 2, 3 };
+            string[] arrayOfStrings = new string[] { "a", "b", "c" };
+
+            Assert.That(arrayOfIntegers, Is.All.Not.Null);
+            Assert.That(arrayOfIntegers, Is.All.GreaterThan(0));
+
+            Assert.That(arrayOfStrings, Is.All.InstanceOf<string>());
+        }
+        #endregion
+
+        #region AllItemsHasExample
+        [Test]
+        public void CanUseHasToTalkAboutAllItems()
+        {
+            int[] arrayOfIntegers = new int[] { 1, 2, 3 };
+
+            Assert.That(arrayOfIntegers, Has.All.GreaterThan(0));
+        }
+        #endregion
+    }
+
+}
