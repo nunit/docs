075b15148571e2114bd8cb61188b3ebf4e20f562
Sean Killeen <SeanKilleen@gmail.com>
2023-12-04 01:10:17 +0000

    Let's see.

--- .devcontainer/get-nunit-apidocs.sh
+++ .devcontainer/get-nunit-apidocs.sh
@@ -1,1 +1,12 @@
 echo "SEAN WAS HERE"
+
+NUNIT_VERSION_FOR_API_DOCS="4.0.0"
+
+first_workspace="$(cd /workspaces && ls | head -1)"
+
+wget "https://github.com/nunit/nunit/releases/download/v$NUNIT_VERSION_FOR_API_DOCS/NUnit.Framework-$NUNIT_VERSION_FOR_API_DOCS.zip" -O apidocs.zip
+
+
+mkdir $first_workspace/code-output 
+unzip apidocs.zip
+cp -r ./apidocs/NUnit.Framework-$NUNIT_VERSION_FOR_API_DOCS/bin/net6.0/* $first_workspace/code-output
