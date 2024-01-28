9abf9fbb965bf5f87a40a2f19aaae04f97cef7a8
Sean Killeen <SeanKilleen@gmail.com>
2024-01-28 00:32:57 -0500

    Additional basic tests

--- docs/snippets/Snippets.NUnitLite/UnitTest1.cs
+++ docs/snippets/Snippets.NUnitLite/UnitTest1.cs
@@ -3,8 +3,24 @@ namespace Snippets.NUnitLite;
 public class Tests
 {
     [Test]
-    public void Test1()
+    public void PassingTest()
     {
         Assert.Pass();
     }
+
+    [Test]
+    [Ignore("Ignored test")]
+    public void IgnoredTest()
+    {
+        Assert.Fail();
+        Assert.Pass();
+    }
+
+    [Test]
+    public void FailingTest()
+    {
+        Assert.Fail();
+    }
+
+
 }
