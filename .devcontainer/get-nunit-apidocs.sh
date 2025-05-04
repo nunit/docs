9d44deac5b693e475e958127b282ce2b6f360c21
Sean Killeen <SeanKilleen@gmail.com>
2025-05-04 17:45:30 +0000

    Update version

--- .devcontainer/get-nunit-apidocs.sh
+++ .devcontainer/get-nunit-apidocs.sh
@@ -1,7 +1,7 @@
 # This script gets the the latest version of NUnit and extracts it. This is because docfx is capable of generating API docs, and this way the Codespace will be automatically enabled to do that.
 
 # This is hard-coded for now.
-NUNIT_VERSION_FOR_API_DOCS="4.0.0"
+NUNIT_VERSION_FOR_API_DOCS="4.3.2"
 
 first_workspace="$(cd /workspaces && ls | head -1)"
 
