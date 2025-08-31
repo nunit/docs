958c87ce3b2e76b9f48e1b51eef555b101fbbe6e
Sean Killeen <SeanKilleen@gmail.com>
2025-08-31 10:46:17 -0400

    Generate snippets from docs

---
+++ docs/snippets/Snippets.NUnit/AttributeExamples.cs
@@ -0,0 +1,346 @@
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+#pragma warning disable CA1822
+#pragma warning disable NUnit2045
+#pragma warning disable CS1998
+
+namespace Snippets.NUnit;
+
+public class AttributeExamples
+{
+    #region ValuesAttributeExample
+    [Test]
+    public void ValuesAttribute_BasicExample([Values(1, 2, 3)] int x, [Values("A", "B")] string s)
+    {
+        Assert.That(x, Is.GreaterThan(0));
+        Assert.That(s, Is.Not.Null);
+    }
+    #endregion
+
+    #region ValuesAttributeEnumExample
+    public enum MyEnumType
+    {
+        Value1,
+        Value2,
+        Value3
+    }
+
+    [Test]
+    public void ValuesAttribute_EnumExample([Values] MyEnumType myEnumArgument)
+    {
+        Assert.That(myEnumArgument, Is.TypeOf<MyEnumType>());
+    }
+    #endregion
+
+    #region ValuesAttributeBoolExample
+    [Test]
+    public void ValuesAttribute_BoolExample([Values] bool value)
+    {
+        // This test will run twice: once with true, once with false
+        Assert.That(value == true || value == false);
+    }
+    #endregion
+
+    #region RangeAttributeExample
+    [Test]
+    public void RangeAttribute_Example(
+        [Values(1, 2, 3)] int x,
+        [Range(0.2, 0.6, 0.2)] double d)
+    {
+        Assert.That(x, Is.GreaterThan(0));
+        Assert.That(d, Is.GreaterThan(0.0));
+    }
+    #endregion
+}
+
+[TestFixture]
+[Category("LongRunning")]
+public class CategoryAttributeExamples
+{
+    #region CategoryTestFixtureExample
+    [Test]
+    public void Test_InLongRunningCategory()
+    {
+        Assert.Pass("This test is in the LongRunning category");
+    }
+    #endregion
+
+    #region CategoryTestExample
+    [Test]
+    [Category("Fast")]
+    public void FastTest()
+    {
+        Assert.Pass("This is a fast test");
+    }
+
+    [Test]
+    [Category("Slow")]
+    [Category("Database")]
+    public void SlowDatabaseTest()
+    {
+        Assert.Pass("This test has multiple categories");
+    }
+    #endregion
+
+    #region CustomCategoryAttribute
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class CriticalAttribute : CategoryAttribute
+    { }
+
+    [Test]
+    [Critical]
+    public void CriticalTest()
+    {
+        Assert.Pass("This test uses a custom category attribute");
+    }
+    #endregion
+}
+
+[TestFixture]
+public class SetupTeardownExamples
+{
+    private int counter;
+
+    #region SetUpExample
+    [SetUp]
+    public void Init()
+    {
+        counter = 0;
+        Console.WriteLine("SetUp method called before each test");
+    }
+    #endregion
+
+    #region TearDownExample
+    [TearDown]
+    public void Cleanup()
+    {
+        Console.WriteLine("TearDown method called after each test");
+    }
+    #endregion
+
+    [Test]
+    public void TestMethod1()
+    {
+        counter++;
+        Assert.That(counter, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TestMethod2()
+    {
+        counter++;
+        Assert.That(counter, Is.EqualTo(1));
+    }
+}
+
+[TestFixture]
+public class OneTimeSetupTeardownExamples
+{
+    private static int staticCounter;
+
+    #region OneTimeSetUpExample
+    [OneTimeSetUp]
+    public void OneTimeInit()
+    {
+        staticCounter = 100;
+        Console.WriteLine("OneTimeSetUp called once before all tests in the fixture");
+    }
+    #endregion
+
+    #region OneTimeTearDownExample
+    [OneTimeTearDown]
+    public void OneTimeCleanup()
+    {
+        Console.WriteLine("OneTimeTearDown called once after all tests in the fixture");
+    }
+    #endregion
+
+    [Test]
+    public void Test1()
+    {
+        Assert.That(staticCounter, Is.EqualTo(100));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.That(staticCounter, Is.EqualTo(100));
+    }
+}
+
+[TestFixture]
+public class DescriptionAttributeExamples
+{
+    #region DescriptionTestFixtureExample
+    [Test]
+    [Description("This test validates the addition operation")]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        Assert.That(2 + 2, Is.EqualTo(4));
+    }
+    #endregion
+
+    #region DescriptionTestExample
+    [Test]
+    [Description("This test should always pass")]
+    public void AlwaysPassingTest()
+    {
+        Assert.Pass("Test passed as expected");
+    }
+    #endregion
+}
+
+[TestFixture]
+public class IgnoreAttributeExamples
+{
+    #region IgnoreTestExample
+    [Test]
+    [Ignore("Test is not ready yet")]
+    public void IgnoredTest()
+    {
+        Assert.Fail("This test is ignored and should not run");
+    }
+    #endregion
+
+    #region IgnoreWithReasonExample
+    [Test]
+    [Ignore("Waiting for bug fix #1234")]
+    public void TestWaitingForBugFix()
+    {
+        Assert.Fail("This test should not run");
+    }
+    #endregion
+}
+
+[TestFixture]
+[Explicit("This fixture is for manual testing only")]
+public class ExplicitAttributeExamples
+{
+    #region ExplicitTestExample
+    [Test]
+    [Explicit("Run this test manually when needed")]
+    public void ManualTest()
+    {
+        Assert.Pass("This test runs only when explicitly requested");
+    }
+    #endregion
+
+    [Test]
+    public void AnotherManualTest()
+    {
+        Assert.Pass("This test is explicit because the fixture is marked explicit");
+    }
+}
+
+[TestFixture]
+public class TimeoutAttributeExamples
+{
+    #region TimeoutExample
+    [Test]
+    [Timeout(1000)] // 1 second timeout
+    public void FastTest()
+    {
+        Thread.Sleep(500); // This should pass as it's under 1 second
+        Assert.Pass();
+    }
+    #endregion
+
+    #region TimeoutWithAsyncExample
+    [Test]
+    [Timeout(2000)] // 2 second timeout
+    public async Task AsyncTestWithTimeout()
+    {
+        await Task.Delay(1000); // This should pass as it's under 2 seconds
+        Assert.Pass();
+    }
+    #endregion
+}
+
+[TestFixture]
+public class RepeatAttributeExamples
+{
+    private int repeatCounter = 0;
+
+    #region RepeatExample
+    [Test]
+    [Repeat(3)]
+    public void RepeatedTest()
+    {
+        repeatCounter++;
+        Console.WriteLine($"Repeat iteration: {repeatCounter}");
+        Assert.Pass();
+    }
+    #endregion
+}
+
+[TestFixture]
+public class OrderAttributeExamples
+{
+    #region OrderExample
+    [Test, Order(1)]
+    public void FirstTest()
+    {
+        Console.WriteLine("This test should run first");
+        Assert.Pass();
+    }
+
+    [Test, Order(2)]
+    public void SecondTest()
+    {
+        Console.WriteLine("This test should run second");
+        Assert.Pass();
+    }
+
+    [Test] // No order specified, will run after ordered tests
+    public void UnorderedTest()
+    {
+        Console.WriteLine("This test has no specific order");
+        Assert.Pass();
+    }
+    #endregion
+}
+
+[TestFixture]
+public class TestOfAttributeExamples
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+    }
+
+    #region TestOfExample
+    [Test]
+    [TestOf(typeof(Calculator))]
+    public void Calculator_Add_ReturnsCorrectSum()
+    {
+        var calculator = new Calculator();
+        Assert.That(calculator.Add(2, 3), Is.EqualTo(5));
+    }
+
+    [Test]
+    [TestOf(typeof(Calculator))]
+    public void Calculator_Subtract_ReturnsCorrectDifference()
+    {
+        var calculator = new Calculator();
+        Assert.That(calculator.Subtract(5, 3), Is.EqualTo(2));
+    }
+    #endregion
+}
+
+[TestFixture]
+public class PropertyAttributeExamples
+{
+    #region PropertyExample
+    [Test]
+    [Property("Author", "John Doe")]
+    [Property("Version", "1.0")]
+    public void TestWithProperties()
+    {
+        Assert.Pass("This test has custom properties");
+    }
+    #endregion
+}
---
+++ docs/snippets/Snippets.NUnit/ConstraintExamples.cs
@@ -0,0 +1,270 @@
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+#pragma warning disable CA1822
+#pragma warning disable NUnit2045
+#pragma warning disable CA2211
+
+namespace Snippets.NUnit;
+
+public class ConstraintExamples
+{
+    #region EqualConstraintNumerics
+    [Test]
+    public void EqualConstraint_Numerics()
+    {
+        Assert.That(2 + 2, Is.EqualTo(4.0));
+        Assert.That(2 + 2 == 4);
+        Assert.That(2 + 2, Is.Not.EqualTo(5));
+        Assert.That(2 + 2 != 5);
+        Assert.That(5.0, Is.EqualTo(5));
+        Assert.That(5.5, Is.EqualTo(5).Within(0.5));
+        Assert.That(5.5, Is.EqualTo(5).Within(10).Percent);
+    }
+    #endregion
+
+    #region EqualConstraintFloatingPoint
+    [Test]
+    public void EqualConstraint_FloatingPoint()
+    {
+        Assert.That(2.1 + 1.2, Is.EqualTo(3.3).Within(.0005));
+        Assert.That(double.PositiveInfinity, Is.EqualTo(double.PositiveInfinity));
+        Assert.That(double.NegativeInfinity, Is.EqualTo(double.NegativeInfinity));
+        Assert.That(double.NaN, Is.EqualTo(double.NaN));
+        Assert.That(20000000000000004.0, Is.EqualTo(20000000000000000.0).Within(1).Ulps);
+    }
+    #endregion
+
+    #region EqualConstraintStrings
+    [Test]
+    public void EqualConstraint_Strings()
+    {
+        Assert.That("Hello!", Is.Not.EqualTo("HELLO!"));
+        Assert.That("Hello!", Is.EqualTo("HELLO!").IgnoreCase);
+
+        string[] expected = new string[] { "Hello", "World" };
+        string[] actual = new string[] { "HELLO", "world" };
+        Assert.That(actual, Is.EqualTo(expected).IgnoreCase);
+    }
+    #endregion
+
+    #region AndConstraintExamples
+    [Test]
+    public void AndConstraint_Examples()
+    {
+        Assert.That(2.3, Is.GreaterThan(2.0).And.LessThan(3.0));
+        
+        int? i = 10;
+        Assert.That(i, Is.Not.Null.And.GreaterThan(9));
+    }
+    #endregion
+
+    #region CollectionContainsExamples
+    [Test]
+    public void CollectionContains_Examples()
+    {
+        int[] iarray = new int[] { 1, 2, 3 };
+        string[] sarray = new string[] { "a", "b", "c" };
+        
+        Assert.That(iarray, Has.Member(3));
+        Assert.That(sarray, Has.Member("b"));
+        Assert.That(sarray, Contains.Item("c"));
+        Assert.That(sarray, Has.No.Member("x"));
+        Assert.That(iarray, Does.Contain(3));
+    }
+    #endregion
+
+    #region GreaterThanExamples
+    [Test]
+    public void GreaterThan_Examples()
+    {
+        Assert.That(7, Is.GreaterThan(3));
+        Assert.That(42, Is.Positive);
+        
+        DateTime expectedDateTime = DateTime.Now.AddMinutes(-1);
+        DateTime myDateTime = DateTime.Now;
+        Assert.That(myDateTime, Is.GreaterThan(expectedDateTime).Within(TimeSpan.FromSeconds(1)));
+    }
+    #endregion
+
+    #region NullConstraintExamples
+    [Test]
+    public void NullConstraint_Examples()
+    {
+        object anObject = null;
+        Assert.That(anObject, Is.Null);
+        
+        anObject = "something";
+        Assert.That(anObject, Is.Not.Null);
+    }
+    #endregion
+
+    #region LessThanExamples
+    [Test]
+    public void LessThan_Examples()
+    {
+        Assert.That(3, Is.LessThan(7));
+        Assert.That(-5, Is.Negative);
+    }
+    #endregion
+
+    #region StringConstraintExamples
+    [Test]
+    public void StringConstraint_Examples()
+    {
+        Assert.That("Hello World!", Does.StartWith("Hello"));
+        Assert.That("Hello World!", Does.EndWith("World!"));
+        Assert.That("Hello World!", Does.Contain("lo Wor"));
+        Assert.That("Hello World!", Does.Match("H.*!"));
+    }
+    #endregion
+
+    #region TrueConstraintExamples
+    [Test]
+    public void TrueConstraint_Examples()
+    {
+        Assert.That(2 + 2 == 4, Is.True);
+        Assert.That(2 + 2 == 5, Is.False);
+    }
+    #endregion
+
+    #region TypeConstraintExamples
+    [Test]
+    public void TypeConstraint_Examples()
+    {
+        Assert.That("Hello", Is.TypeOf<string>());
+        Assert.That("Hello", Is.TypeOf(typeof(string)));
+        Assert.That("Hello", Is.InstanceOf<string>());
+        Assert.That("Hello", Is.InstanceOf<object>());
+        Assert.That("Hello", Is.AssignableTo<object>());
+    }
+    #endregion
+
+    #region RangeConstraintExamples
+    [Test]
+    public void RangeConstraint_Examples()
+    {
+        int[] iarray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Assert.That(5, Is.InRange(1, 10));
+        Assert.That(42, Is.Not.InRange(1, 10));
+        Assert.That(iarray, Has.All.InRange(1, 10));
+    }
+    #endregion
+
+    #region CollectionConstraintExamples
+    [Test]
+    public void CollectionConstraint_Examples()
+    {
+        int[] iarray = { 1, 2, 3 };
+        string[] sarray = { "a", "b", "c" };
+        
+        Assert.That(iarray, Is.All.GreaterThan(0));
+        Assert.That(sarray, Is.All.InstanceOf<string>());
+        Assert.That(iarray, Has.Some.EqualTo(2));
+        Assert.That(iarray, Has.None.EqualTo(4));
+        Assert.That(iarray, Is.Unique);
+        Assert.That(new int[] {}, Is.Empty);
+        Assert.That(iarray, Has.Exactly(3).Items);
+        Assert.That(iarray, Has.Some.GreaterThan(2));
+    }
+    #endregion
+
+    #region FileConstraintExamples
+    [Test]
+    public void FileConstraint_Examples()
+    {
+        var tempFile = Path.GetTempFileName();
+        File.WriteAllText(tempFile, "test content");
+        
+        try
+        {
+            Assert.That(tempFile, Does.Exist);
+            Assert.That("nonexistent.txt", Does.Not.Exist);
+        }
+        finally
+        {
+            if (File.Exists(tempFile))
+                File.Delete(tempFile);
+        }
+    }
+    #endregion
+
+    #region PropertyConstraintExamples
+    public class Person
+    {
+        public string Name { get; set; } = "";
+        public int Age { get; set; }
+        public string Email { get; set; } = "";
+    }
+
+    [Test]
+    public void PropertyConstraint_Examples()
+    {
+        var person = new Person { Name = "John", Age = 25, Email = "john@example.com" };
+        
+        Assert.That(person, Has.Property("Name").EqualTo("John"));
+        Assert.That(person, Has.Property("Age").GreaterThan(18));
+        Assert.That(person, Has.Property("Email").Contains("@"));
+    }
+    #endregion
+
+    #region EmptyConstraintExamples
+    [Test]
+    public void EmptyConstraint_Examples()
+    {
+        Assert.That("", Is.Empty);
+        Assert.That(new int[] {}, Is.Empty);
+        Assert.That(new List<string>(), Is.Empty);
+        Assert.That("Hello", Is.Not.Empty);
+    }
+    #endregion
+
+    #region OrConstraintExamples
+    [Test]
+    public void OrConstraint_Examples()
+    {
+        Assert.That(3, Is.LessThan(5).Or.GreaterThan(10));
+        Assert.That(12, Is.LessThan(5).Or.GreaterThan(10));
+    }
+    #endregion
+
+    #region NotConstraintExamples
+    [Test]
+    public void NotConstraint_Examples()
+    {
+        Assert.That(42, Is.Not.EqualTo(99));
+        Assert.That("Hello", Is.Not.Null);
+        Assert.That(new int[] { 1, 2, 3 }, Is.Not.Empty);
+    }
+    #endregion
+
+    #region ThrowsConstraintExamples
+    [Test]
+    public void ThrowsConstraint_Examples()
+    {
+        Assert.That(() => throw new ArgumentException(), Throws.ArgumentException);
+        Assert.That(() => throw new InvalidOperationException("test"), 
+                   Throws.InvalidOperationException.With.Message.EqualTo("test"));
+        Assert.That(() => { }, Throws.Nothing);
+    }
+    #endregion
+
+    #region DelayedConstraintExamples
+    [Test]
+    public void DelayedConstraint_Examples()
+    {
+        bool flag = false;
+        Task.Run(async () => 
+        { 
+            await Task.Delay(100); 
+            flag = true; 
+        });
+        
+        Assert.That(() => flag, Is.True.After(200).MilliSeconds);
+    }
+    #endregion
+}
